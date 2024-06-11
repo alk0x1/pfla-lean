19484beab2d0264fb6577ac7543efcf080adc0cf
Alan Bertani <alanbertani0x01@gmail.com>
2024-06-11 01:57:29 -0300

    add: implementing subst

--- Plfa/PLF/lambda.lean
+++ Plfa/PLF/lambda.lean
@@ -16,6 +16,7 @@ def a : Identifier := "a"
 def b : Identifier := "b"
 def x : Identifier := "x"
 def y : Identifier := "y"
+def z : Identifier := "z"
 def m : Identifier := "m"
 def n : Identifier := "n"
 def f : Identifier := "f"
@@ -27,6 +28,31 @@ def true_term : Term := lam x (lam y (var x)) --λx.λy.x
 def false_term : Term := lam x (lam y (var y)) -- λx.λy.y
 def if_term : Term := lam p (lam a (lam b (app (app (var p) (var a)) (var b)))) -- λp.λa.λb.pab
 def add : Term := lam m (lam n (lam f (lam x (app (app (var m) (var f)) (app (app (var n) (var f)) (var x)) )))) -- λm.λn.λf.λx.mf(nfx)
+
+-- Substitution function
+def subst (x : Identifier) (v : Term) : Term → Term
+| (var y) => if x = y then v else var y
+| (lam y t) => if x = y then lam y t else lam y (subst x v t)
+| (app t1 t2) => var x
+| zero => zero
+| (suc t) => var x
+| (case t1 t2 y t3) => var x
+| (mu y t) => var x
+
+-- Testing Substitutions
+def term1 := var x
+def term2 := lam y (var x)
+
+def v := lam z (var z)
+def test1 := subst x v term1 -- x -> v in x
+
+
+
+
+-- Normalizer
+-- def normalizer (term : Term) :=
+
+
 
 def two : Term :=
   suc (suc zero)

ba020a482de681dfade9a9a95c9aff30bce61602
Alan Bertani <alanbertani0x01@gmail.com>
2024-06-11 17:21:15 -0300

    update eval

--- Plfa/PLF/lambda.lean
+++ Plfa/PLF/lambda.lean
@@ -1,5 +1,12 @@
+import Init.Data.Repr
+import Init.Data.String.Basic -- Ensure we have access to string manipulation functions
+
 def Identifier := String
 instance : DecidableEq Identifier := inferInstanceAs (DecidableEq String)
+
+-- Repr instance for Identifier
+instance : Repr Identifier where
+  reprPrec x _ := Std.Format.text x
 
 inductive Term
 | var   : Identifier → Term
@@ -11,77 +18,57 @@ inductive Term
 | mu    : Identifier → Term → Term
 open Term
 
-def p : Identifier := "p"
-def a : Identifier := "a"
-def b : Identifier := "b"
-def x : Identifier := "x"
-def y : Identifier := "y"
-def z : Identifier := "z"
-def m : Identifier := "m"
-def n : Identifier := "n"
-def f : Identifier := "f"
-
-def identity_term : Term := lam x (var x) -- λx.x
-def const : Term := lam x (lam y (var x)) -- λx.λy.x
-def self_app : Term := lam x (app (var x) (var x)) --λx.xx
-def true_term : Term := lam x (lam y (var x)) --λx.λy.x
-def false_term : Term := lam x (lam y (var y)) -- λx.λy.y
-def if_term : Term := lam p (lam a (lam b (app (app (var p) (var a)) (var b)))) -- λp.λa.λb.pab
-def add : Term := lam m (lam n (lam f (lam x (app (app (var m) (var f)) (app (app (var n) (var f)) (var x)) )))) -- λm.λn.λf.λx.mf(nfx)
+-- Repr instance for Term
+instance : Repr Term where
+  reprPrec
+  | var x, _ => Std.Format.bracket "var " (Std.Format.text x) ""
+  | lam x t, _ => Std.Format.bracket "(" (Std.Format.text ("lam " ++ x ++ " . ") ++ repr t) ")"
+  | app t1 t2, _ => Std.Format.bracket "(" (repr t1 ++ Std.Format.text " " ++ repr t2) ")"
+  | zero, _ => Std.Format.text "zero"
+  | suc t, _ => Std.Format.bracket "(" (Std.Format.text "suc " ++ repr t) ")"
+  | case t1 t2 x t3, _ => Std.Format.bracket "(" (Std.Format.text ("case " ++ repr t1 ++ " " ++ repr t2 ++ " " ++ x ++ " " ++ repr t3)) ")"
+  | mu x t, _ => Std.Format.bracket "(" (Std.Format.text ("mu " ++ x ++ " . " ++ repr t)) ")"
 
 -- Substitution function
 def subst (x : Identifier) (v : Term) : Term → Term
 | (var y) => if x = y then v else var y
 | (lam y t) => if x = y then lam y t else lam y (subst x v t)
-| (app t1 t2) => var x
+| (app t1 t2) => app (subst x v t1) (subst x v t2)
 | zero => zero
-| (suc t) => var x
-| (case t1 t2 y t3) => var x
-| (mu y t) => var x
+| (suc t) => suc (subst x v t)
+| (case t1 t2 y t3) => case (subst x v t1) (subst x v t2) y (subst x v t3)
+| (mu y t) => if x = y then mu y t else mu y (subst x v t)
 
--- Testing Substitutions
+-- Example Identifiers
+def x : Identifier := "x"
+def y : Identifier := "y"
+def z : Identifier := "z"
+
+-- Example Terms
 def term1 := var x
 def term2 := lam y (var x)
+def term3 := app (var x) (var y)
+def term4 := lam x (var x)
+def term5 := lam z (app (var x) (var y))
+def term6 := app (lam y (var x)) (lam z (var z))
+def term7 := lam y (app (var x) (var y))
 
+-- Example substitution
 def v := lam z (var z)
+
+-- Substitution Examples
 def test1 := subst x v term1 -- x -> v in x
+def test2 := subst x v term2 -- x -> v in (\y.x)
+def test3 := subst x v term3 -- x -> v in (x y)
+def test4 := subst x v term4 -- x -> v in (\x.x)
+def test5 := subst x v term5 -- x -> v in (\z.(x y))
+def test6 := subst x v term6 -- x -> v in ((\y.x) (\z.z))
+def test7 := subst x v term7 -- x -> v in (\y.(x y))
 
-
-
-
--- Normalizer
--- def normalizer (term : Term) :=
-
-
-
-def two : Term :=
-  suc (suc zero)
-
-def four : Term :=
-  suc (suc (suc (suc zero)))
-
-def plus : Term :=
-  mu "+" (
-    lam "m" (
-      lam "n" (
-        case (var "m")
-          (var "n")
-          "m"
-          (suc ( app ( app (var "+") (var "m") ) (var "n") ))
-      )
-    )
-  )
-
-
-
-
--- Environment
-def Env := List (Identifier × Term)
-
-def lookup (env : Env) (x : Identifier) : Option Term :=
-  match env with
-  | [] => none
-  | (y, v) :: ys => if x = y then some v else lookup ys x
-
-def extend (env : Env) (x : Identifier) (v : Term) : Env :=
-  (x, v) :: env
+#eval test1 -- Expected: var (lam z . var z)
+#eval test2 -- Expected: lam y . var (lam z . var z)
+#eval test3 -- Expected: app (lam z . var z) var y
+#eval test4 -- Expected: lam x . var x
+#eval test5 -- Expected: lam z . app (lam z . var z) var y
+#eval test6 -- Expected: app (lam y . var (lam z . var z)) (lam z . var z)
+#eval test7 -- Expected: lam y . app (lam z . var z) var y
